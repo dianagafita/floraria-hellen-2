d0e421197715bd0541f3cb98f3a4919f1bbe22b2
dianagafita <gafita.diana12@gmail.com>
2024-09-20 00:15:40 +0300

    photo next

--- components/pages/imageModal.js
+++ components/pages/imageModal.js
@@ -1,22 +1,34 @@
 import React from "react";
-import Image from "next/image";
+import { MdClose, MdArrowForward, MdArrowBack } from "react-icons/md";
 
-export function ImageModal({ isOpen, imgSrc, onClose }) {
+export const ImageModal = ({ isOpen, imgSrc, onClose, onNext, onPrev }) => {
   if (!isOpen) return null;
 
   return (
-    <div
-      onClick={onClose}
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-    >
-      <div className="relative p-5 rounded-sm w-[90vw] h-[90vw] md:w-[70vw] md:h-[70vw] lg:h-[40vw]">
-        <Image
+    <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-75">
+      <button
+        className="absolute z-[60]  left-2 top-1/2 transform -translate-y-1/2 text-white"
+        onClick={onPrev}
+      >
+        <MdArrowBack size={30} />
+      </button>
+      <div className="relative">
+        <button className="absolute top-2 right-2 text-white" onClick={onClose}>
+          <MdClose size={30} />
+        </button>
+
+        <img
           src={imgSrc}
-          alt="Full size image"
-          layout="fill"
-          className="rounded-sm  object-contain"
+          alt="Modal content"
+          className="max-h-[80vh] max-w-[90vw]"
         />
       </div>
+      <button
+        className="absolute right-2 top-1/2 transform -translate-y-1/2 text-white"
+        onClick={onNext}
+      >
+        <MdArrowForward size={30} />
+      </button>
     </div>
   );
-}
+};
--- components/pages/photoGallery.js
+++ components/pages/photoGallery.js
@@ -1,3 +1,78 @@
+// // "use client";
+// // import { useState } from "react";
+// // import Image from "next/image";
+// // import { ImageModal } from "./imageModal";
+
+// // export function FeaturedImageGallery({ images, type }) {
+// //   const [active, setActive] = useState(images[0]);
+// //   const [isModalOpen, setIsModalOpen] = useState(false);
+// //   const [modalImage, setModalImage] = useState(null);
+
+// //   const handleImageClick = (imgelink) => {
+// //     setModalImage(imgelink);
+// //     setIsModalOpen(true);
+// //   };
+
+// //   const closeModal = () => {
+// //     setIsModalOpen(false);
+// //     setModalImage(null);
+// //   };
+
+// //   return (
+// //     <>
+// //       <div
+// //         className={`${
+// //           type === "event" ? "grid-cols-1" : "grid-cols-3"
+// //         } grid  md:grid-cols-1 gap-1 md:gap-4 w-full `}
+// //       >
+// //         <div className="col-span-2 md:col-span-1">
+// //           <Image
+// //             src={active}
+// //             layout="responsive"
+// //             width={800}
+// //             height={600}
+// //             alt="Gallery image"
+// //             className="object-cover"
+// //           />
+// //         </div>
+// //         <div
+// //           className={`w-full ${
+// //             type === "event"
+// //               ? "flex "
+// //               : "grid-cols-1 grid  md:grid-cols-5  gap-1"
+// //           } `}
+// //         >
+// //           {images.map((imgelink, index) => (
+// //             <div
+// //               key={index}
+// //               className={`${type === "event" ? "h-24 " : "h-1/2 "}  md:h-24`}
+// //             >
+// //               <Image
+// //                 onClick={() => handleImageClick(imgelink)}
+// //                 src={imgelink}
+// //                 layout={type === "event" ? "" : "responsive"} // Adjust the second option as needed
+// //                 width={500}
+// //                 height={200}
+// //                 className="cursor-pointer  rounded-sm object-cover object-center w-full h-full"
+// //                 alt="Gallery image"
+// //               />
+// //             </div>
+// //           ))}
+// //         </div>
+// //       </div>
+
+// //       {type !== "event" ? (
+// //         ""
+// //       ) : (
+// //         <ImageModal
+// //           isOpen={isModalOpen}
+// //           imgSrc={modalImage}
+// //           onClose={closeModal}
+// //         />
+// //       )}
+// //     </>
+// //   );
+// // }
 // "use client";
 // import { useState } from "react";
 // import Image from "next/image";
@@ -23,7 +98,7 @@
 //       <div
 //         className={`${
 //           type === "event" ? "grid-cols-1" : "grid-cols-3"
-//         } grid  md:grid-cols-1 gap-1 md:gap-4 w-full `}
+//         } grid  md:grid-cols-1 gap-1 md:gap-4 w-full`}
 //       >
 //         <div className="col-span-2 md:col-span-1">
 //           <Image
@@ -32,28 +107,34 @@
 //             width={800}
 //             height={600}
 //             alt="Gallery image"
-//             className="object-cover"
+//             className={`object-cover ${
+//               type === "event" ? "min-h-[350px] min-w-[350px]" : ""
+//             }`}
 //           />
 //         </div>
 //         <div
-//           className={`w-full ${
+//           className={`w-full md:min-w-[350px] ${
 //             type === "event"
-//               ? "flex "
-//               : "grid-cols-1 grid  md:grid-cols-5  gap-1"
-//           } `}
+//               ? "flex overflow-x-auto "
+//               : "grid grid-cols-1 md:grid-cols-5 gap-1"
+//           }`}
 //         >
 //           {images.map((imgelink, index) => (
 //             <div
 //               key={index}
-//               className={`${type === "event" ? "h-24 " : "h-1/2 "}  md:h-24`}
+//               className={`${
+//                 type === "event"
+//                   ? "h-24 flex-shrink-0 overflow-hidden w-40 mr-1"
+//                   : "h-1/2"
+//               } md:h-24`}
 //             >
 //               <Image
 //                 onClick={() => handleImageClick(imgelink)}
 //                 src={imgelink}
-//                 layout={type === "event" ? "" : "responsive"} // Adjust the second option as needed
+//                 layout={type === "event" ? "intrinsic" : "responsive"}
 //                 width={500}
 //                 height={200}
-//                 className="cursor-pointer  rounded-sm object-cover object-center w-full h-full"
+//                 className="cursor-pointer rounded-sm object-cover object-center w-full h-full"
 //                 alt="Gallery image"
 //               />
 //             </div>
@@ -61,9 +142,7 @@
 //         </div>
 //       </div>
 
-//       {type !== "event" ? (
-//         ""
-//       ) : (
+//       {isModalOpen && (
 //         <ImageModal
 //           isOpen={isModalOpen}
 //           imgSrc={modalImage}
@@ -81,16 +160,27 @@ import { ImageModal } from "./imageModal";
 export function FeaturedImageGallery({ images, type }) {
   const [active, setActive] = useState(images[0]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalImage, setModalImage] = useState(null);
+  const [modalImageIndex, setModalImageIndex] = useState(0);
 
-  const handleImageClick = (imgelink) => {
-    setModalImage(imgelink);
+  const handleImageClick = (index) => {
+    setModalImageIndex(index);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
-    setModalImage(null);
+  };
+
+  const handleNext = () => {
+    setModalImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrev = () => {
+    setModalImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
   };
 
   return (
@@ -123,11 +213,13 @@ export function FeaturedImageGallery({ images, type }) {
             <div
               key={index}
               className={`${
-                type === "event" ? "h-24 flex-shrink-0 w-40 mr-1" : "h-1/2"
+                type === "event"
+                  ? "h-24 flex-shrink-0 overflow-hidden w-40 mr-1"
+                  : "h-1/2"
               } md:h-24`}
             >
               <Image
-                onClick={() => handleImageClick(imgelink)}
+                onClick={() => handleImageClick(index)}
                 src={imgelink}
                 layout={type === "event" ? "intrinsic" : "responsive"}
                 width={500}
@@ -143,8 +235,10 @@ export function FeaturedImageGallery({ images, type }) {
       {isModalOpen && (
         <ImageModal
           isOpen={isModalOpen}
-          imgSrc={modalImage}
+          imgSrc={images[modalImageIndex]}
           onClose={closeModal}
+          onNext={handleNext}
+          onPrev={handlePrev}
         />
       )}
     </>
