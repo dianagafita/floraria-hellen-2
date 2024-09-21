97e440cefbc5c0ef95e23e86d82aa9cb85339602
dianagafita <105122874+dianagafita@users.noreply.github.com>
2024-09-21 18:28:32 +0300

    Update event-product-actions.js

--- actions/event-product-actions.js
+++ actions/event-product-actions.js
@@ -2,13 +2,7 @@
 
 import { uploadImages } from "@/lib/claudinary";
 import prisma from "@/lib/prisma";
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb', // Adjust size limit as needed
-    },
-  },
-};
+
 export async function addEventProduct(prevState, formData) {
   const productName = formData.get("productName");
   const productType = formData.get("productType");
