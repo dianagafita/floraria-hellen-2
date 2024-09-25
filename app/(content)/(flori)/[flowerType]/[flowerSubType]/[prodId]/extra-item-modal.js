db9c904430210f7601cbef077c4084b675c9c25c
dianagafita <gafita.diana12@gmail.com>
2024-09-25 14:06:00 +0300

    ok

--- app/(content)/(flori)/[flowerType]/[flowerSubType]/[prodId]/extra-item-modal.js
+++ app/(content)/(flori)/[flowerType]/[flowerSubType]/[prodId]/extra-item-modal.js
@@ -5,20 +5,32 @@ export default function ExtraItemModal({ item, onClose, onAdd }) {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="m-10 bg-white p-5 rounded-sm ">
-        <h2 className="text-xl font-bold" translate="no">
+      <div className="m-10 bg-white p-10 rounded-sm ">
+        <h2
+          className="text-xl md:text-2xl xl:text-3xl font-bold"
+          translate="no"
+        >
           {item.name}
         </h2>
-        <img src={item.image} alt={item.name} className="w-40 h-40 my-3" />
-        <p>{item.description}</p>
-        <p>
+        <img src={item.image} alt={item.name} className="h-40 my-3" />
+        <p className="md:text-lg xl:text-2xl">{item.description}</p>
+        <p className="md:text-lg xl:text-xl my-2">
           Produsul se poate comanda doar impreuna cu un buchet de flori sau un
           aranajment!
         </p>
-        <p className="font-bold my-2">{item.price} lei</p>
-        <div className="flex space-x-3 mt-5">
-          <Button onClick={() => onAdd(item)}>Add to Cart</Button>
-          <Button onClick={onClose}>Cancel</Button>
+        <p className="font-bold text-end text-base md:text-lg xl:text-2xl  my-5">
+          {item.price} lei
+        </p>
+        <div className="flex space-x-3 mt-10 mb-5 justify-between">
+          <Button
+            moreStyle="px-6 md:text-lg xl:text-2xl"
+            onClick={() => onAdd(item)}
+          >
+            Adauga
+          </Button>
+          <Button moreStyle="px-6 md:text-lg xl:text-2xl" onClick={onClose}>
+            Inchide
+          </Button>
         </div>
       </div>
     </div>
