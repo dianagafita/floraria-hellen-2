bc5ba2a65282f58c84aea36e4383a01c82c01a99
dianagafita <gafita.diana12@gmail.com>
2024-10-20 11:55:31 +0300

    oks

--- app/(content)/evenimente/nunta/[weddingComponent]/ims.js
+++ app/(content)/evenimente/nunta/[weddingComponent]/ims.js
@@ -22,13 +22,13 @@ const FlowerImage = ({ src, alt, href }) => {
         priority
         className={`rounded-sm object-cover overflow-hidden ${
           isVertical
-            ? "h-full w-full w-[250px] min-h-[300px] max-h-[350px]"
+            ? "h-full w-[250px] min-h-[300px] max-h-[350px]"
             : "h-[250px] w-full min-w-[450px]"
         }`}
         src={src}
         alt={alt}
-        width={isVertical ? 400 : 400}
-        height={isVertical ? 500 : 300}
+        width={isVertical ? 250 : 400}
+        height={isVertical ? 350 : 300}
         onLoadingComplete={handleLoadingComplete}
       />
       <span className="tracking-widest absolute inset-0 flex items-center justify-center md:bg-gradient-to-r from-[rgba(0,0,0,0.3)] to-white-600 text-white text-center opacity-0 translate-x-[-100%] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 ease-in-out">
