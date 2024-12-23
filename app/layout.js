919df04ee95726e01efa519ebe185d347d106479
dianagafita <gafita.diana12@gmail.com>
2024-12-23 23:52:30 +0200

    ok

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,15 @@
 import { CartProvider } from "@/context/cart-context";
 import "./globals.css";
 import { AuthProvider } from "@/context/auth-context";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
     <AuthProvider>
-      <CartProvider>{children}</CartProvider>
+      <CartProvider>
+        {children}
+        <Analytics />
+      </CartProvider>
     </AuthProvider>
   );
 }
