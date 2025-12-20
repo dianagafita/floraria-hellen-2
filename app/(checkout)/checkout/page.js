5deccdc1ecb01d5b684991d9ae387797183e6795
dianagafita <gafita.diana12@gmail.com>
2025-12-20 15:56:45 +0200

    transport +1

--- app/(checkout)/checkout/page.js
+++ app/(checkout)/checkout/page.js
@@ -48,7 +48,7 @@ export default function CheckoutPage() {
 
   useEffect(() => {
     if (distance !== null) {
-      const fee = Math.round((distance / 1000) * 3 + 10);
+      const fee = Math.round((distance / 1000) * 3 + 11);
       setShippingFee(fee);
     }
   }, [distance]);
