0374ea9b2088584bc3e35fcfb07726f311c2a4a8
dianagafita <gafita.diana12@gmail.com>
2025-12-20 16:35:51 +0200

    image fix

--- app/(payment-success)/payment-success/page.js
+++ app/(payment-success)/payment-success/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import img from "./martie.jpeg";
+import img from "./christmas.jpg";
 
 export default function PaymentSuccessPage() {
   const [orderUpdated, setOrderUpdated] = useState(false);
